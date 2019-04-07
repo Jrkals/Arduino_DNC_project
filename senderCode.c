e8a3b7905c9124cf72a50ea5b5f86c8f320562d0
Jrkals <jkalan@udallas.edu>
2019-04-07 16:29:05 -0500

    Initial Sender code
    Just a wish list of functions. The big thing is getting the code words made

---
+++ senderCode.c
@@ -0,0 +1,32 @@
+// Sender code
+
+//global state variables
+char* currentCharacter;
+int arrayOfCodeWordsBase10[125];
+char* arrayOfCodeWordsBase5[125];
+
+
+int main(){
+
+  return 0;
+}
+
+char* convertCharToBase5(){
+
+}
+
+char* convertBase5ToCodeWord(){
+
+}
+
+int convertBase5ToBase10(){
+
+}
+
+char* convertBase10ToBase5(){
+
+}
+
+char* convertBase5ToLight(){
+  
+}
